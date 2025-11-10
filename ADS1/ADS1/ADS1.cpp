24c87fcf1bda7e6cb48fe1e3016750cdc1b144e5
MrDiwad <d.rutkowski2006@gmail.com>
2025-11-10 15:40:51 +0100

    ADS1 commit

---
+++ ADS1/ADS1/ADS1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+int main() {
+    int numbers[5] = { 10, 20, 30, 40, 50 };
+    int i = 0;
+    //int length = sizeof(numbers) / sizeof(int);
+    numbers[5] = 7;
+    cout << numbers[5] << "\n";
+    int length = sizeof(numbers) / sizeof(int);
+    //using while loop to access all
+    //elements of the array
+    while (i < length) {
+        cout << numbers[i] << "\n";
+        i++;
+    }
+    return 0;
+}
+
+*/
+
+// for a given array name suma of kwadraty elements
+
+int main() {
+    int numbers[5] = { 1,100,10000,1000000,10000000 };
+    //int tab[0];
+    int length = sizeof(numbers) / sizeof(numbers[0]);
+    if (length > 0) {
+        long long int suma = 0;
+
+        for (int i = 0; i < length; i++) {
+            suma += (long long int)numbers[i] * (long long int)numbers[i];
+        }
+        cout << suma << endl;
+    }
+    else {
+        cout << "Tab length is <= 0" << endl;
+    }
+   
+}
